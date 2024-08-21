3e152cfeaaf5c115724b625e521c95d01c53b261
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2024-08-21 17:00:49 -0300

    developing comments on question and answers

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,8 +1,17 @@
+import { PaginationParams } from 'src/core/repositories/pagination-params'
 import { AnswersRepository } from 'src/domain/forum/application/repositories/answers-repository'
 import { Answer } from 'src/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
 
   async save(answer: Answer) {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,8 +1,17 @@
+import { PaginationParams } from 'src/core/repositories/pagination-params'
 import { QuestionsRepository } from 'src/domain/forum/application/repositories/question-repository'
 import { Question } from 'src/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
 
   async save(question: Question) {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
